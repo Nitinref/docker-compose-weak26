751f3abe43151cc1996786e884499c981616aeb1
nitin yadav <nitinyadav484220@gmail.com>
2025-07-01 12:07:58 +0530

    added

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express  from "express";
+import { PrismaClient } from "./generated/prisma/index.js";
+const app = express()
+const prismaClient = new PrismaClient()
+
+app.get('/' ,async (req,res) =>{
+   
+ const data =   await prismaClient.user.findMany()
+
+    res.json({
+        "message":"get endpoint",
+        data
+    })
+})
+
+app.post("/" , async (req,res)=>{
+
+     await prismaClient.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
+
+    res.json({
+        "message":"post endponit "
+    })
+})
+app.listen(3000)
